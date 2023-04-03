b5d3abae1231ede71c56dd63a8ad4402bf04b998
MartinovG <gabismart7@yahoo.com>
2023-04-03 23:08:12 +0300

    adding pptx and more code

--- gui.py
+++ gui.py
@@ -3,30 +3,29 @@ import random
 
 pygame.init()
 
-# Set the size of the screen
-screen_width = 800
-screen_height = 600
+screen_width = 1000
+screen_height = 700
 screen = pygame.display.set_mode((screen_width, screen_height))
 snake_timer = 0
 wolf_timer = 0
 rabbit_timer = 0
 fox_timer = 0
 bear_timer = 0
+border_color = ('#808080')
+border_thickness = 2
 
-# Set the title of the window
-pygame.display.set_caption("Random Square")
+pygame.draw.rect(screen, border_color, (290, 10, 700, 400), border_thickness)
 
-# Set the clock
+pygame.display.set_caption("Ecosystem Simulator")
+
 clock = pygame.time.Clock()
 
-# Define colors
 color1 = ('#0000FF') # Blue
 color2 = ('#FF0000') # Red
 color3 = ('#00FF00') # Green
 color4 = ('#FFFFFF') # White
 color5 = ('#FFFF00') #Yellow
 
-# Define functions to draw squares
 def snake(x, y, color):
     for i in range(x, x+10):
         for j in range(y, y+10):
@@ -52,19 +51,15 @@ def bear(x, y, color):
         for j in range(y, y+10):
             screen.set_at((i, j), color)
 
-# Main game loop
 while True:
-    # Handle events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             quit()
 
-    # Generate a random square position and color
-    x = random.randint(0, screen_width - 10)
-    y = random.randint(0, screen_height - 10)
+    x = random.randint(300, screen_width - 20)
+    y = random.randint(20, screen_height - 300)
 
-    # Draw a random square
     if random.random() < 0.2:
         if random.random() < 0.2:
             snake(x, y, color1)
@@ -77,8 +72,6 @@ while True:
         else:
             bear(x, y, color5)
 
-    # Update the display
     pygame.display.flip()
 
-    # Tick the clock
     clock.tick(60)
