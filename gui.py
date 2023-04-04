0ec670d5a493f820de6914665a9eea126c7244bd
MartinovG <gabismart7@yahoo.com>
2023-04-04 11:56:35 +0300

    adding start/stop button

--- gui.py
+++ gui.py
@@ -6,13 +6,12 @@ pygame.init()
 screen_width = 1000
 screen_height = 700
 screen = pygame.display.set_mode((screen_width, screen_height))
-snake_timer = 0
-wolf_timer = 0
-rabbit_timer = 0
-fox_timer = 0
-bear_timer = 0
-border_color = ('#808080')
+border_color = ('#808080') # Grey
 border_thickness = 2
+font = pygame.font.Font(None, 36)
+button_rect = pygame.Rect(100, 100, 200, 50)
+button_color = ('#808080')
+button_text = "Start!"
 
 pygame.draw.rect(screen, border_color, (290, 10, 700, 400), border_thickness)
 
@@ -25,6 +24,8 @@ color2 = ('#FF0000') # Red
 color3 = ('#00FF00') # Green
 color4 = ('#FFFFFF') # White
 color5 = ('#FFFF00') #Yellow
+
+spawning = False
 
 def snake(x, y, color):
     for i in range(x, x+10):
@@ -51,26 +52,46 @@ def bear(x, y, color):
         for j in range(y, y+10):
             screen.set_at((i, j), color)
 
+
+def draw_button():
+    button_surf = pygame.Surface(button_rect.size)
+    button_surf.fill(button_color)
+    screen.blit(button_surf, button_rect)
+    
+    text_surf = font.render(button_text, True, (255, 255, 255))
+    text_rect = text_surf.get_rect(center=button_rect.center)
+    screen.blit(text_surf, text_rect)
+
+def button_click():
+    global spawning
+    spawning = not spawning
+
+draw_button()
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             quit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if button_rect.collidepoint(event.pos):
+                button_click()
 
-    x = random.randint(300, screen_width - 20)
-    y = random.randint(20, screen_height - 300)
-
-    if random.random() < 0.2:
+    if spawning:
+        x = random.randint(300, screen_width - 20)
+        y = random.randint(20, screen_height - 300)
         if random.random() < 0.2:
-            snake(x, y, color1)
-        elif random.random() < 0.4:
-            wolf(x, y, color2)
-        elif random.random() < 0.6:
-            rabbit(x, y, color3)
-        elif random.random() < 0.8:
-            fox(x, y, color4)
-        else:
-            bear(x, y, color5)
+            if random.random() < 0.2:
+                snake(x, y, color1)
+            elif random.random() < 0.4:
+                wolf(x, y, color2)
+            elif random.random() < 0.6:
+                rabbit(x, y, color3)
+            elif random.random() < 0.8:
+                fox(x, y, color4)
+            else:
+                bear(x, y, color5)
+        pygame.display.update()
 
     pygame.display.flip()
 
