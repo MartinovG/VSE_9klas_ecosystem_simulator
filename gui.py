2336ab690ad5cdb257068145e6de2c657ea8b64c
MartinovG <gabismart7@yahoo.com>
2023-05-04 16:54:06 +0300

    done with moving/eating

--- gui.py
+++ gui.py
@@ -3,8 +3,10 @@ import pygame.gfxdraw
 import random
 import math
 
+# initialize Pygame
 pygame.init()
 
+# define constants
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 FPS = 60
@@ -14,21 +16,32 @@ HERBIVORE_COLOR = (0, 0, 255) # Red
 PREDATOR_COLOR = (255, 0, 0) # Blue
 HERBIVORE_SPEED = 2
 HERBIVORE_ENERGY = 50
+HERBIVORE_SIGHT_RANGE = 100
+HERBIVORE_FOOD_THRESHOLD = 50
 PREDATOR_SPEED = 3
 PREDATOR_ENERGY = 200
+PREDATOR_SIGHT_RANGE = 150
+PREDATOR_FOOD_THRESHOLD = 100
+PLANT_ENERGY = 100
+PLANT_GROWTH_RATE = 0.1
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("2D Ecosystem Simulator")
 clock = pygame.time.Clock()
 
 class Plant:
-    def __init__(self, x, y):
+    def __init__(self, x, y, energy):
         self.x = x
         self.y = y
+        self.energy = energy
 
-    def draw(self, screen ):
-        pygame.gfxdraw.filled_circle(screen, self.x, self.y, 5, PLANT_COLOR)
-        pygame.gfxdraw.aacircle(screen, self.x, self.y, 5, PLANT_COLOR)
+    def draw(self, screen):
+        radius = max(2, min(5, int(self.energy / 20)))
+        pygame.gfxdraw.filled_circle(screen, self.x, self.y, radius, PLANT_COLOR)
+        pygame.gfxdraw.aacircle(screen, self.x, self.y, radius, PLANT_COLOR)
+
+    def grow(self):
+        self.energy = min(self.energy + PLANT_GROWTH_RATE * self.energy, 100)
 
 class Herbivore:
     def __init__(self, x, y, energy):
@@ -46,9 +59,24 @@ class Herbivore:
         dy = HERBIVORE_SPEED * math.sin(self.direction)
         self.x += dx
         self.y += dy
-    
+
     def turn(self):
         self.direction += random.uniform(-math.pi / 16, math.pi / 16)
+
+    def eat(self, plant):
+        distance = math.sqrt((self.x - plant.x) ** 2 + (self.y - plant.y) ** 2)
+        if distance <= 15:
+            self.energy += plant.energy
+            return True
+        else:
+            return False
+
+    def reproduce(self):
+        if self.energy >= HERBIVORE_FOOD_THRESHOLD:
+            self.energy /= 2
+            return Herbivore(self.x, self.y, self.energy)
+        else:
+            return None
 
 class Predator:
     def __init__(self, x, y, energy):
@@ -58,43 +86,88 @@ class Predator:
         self.direction = random.uniform(0, 2 * math.pi)
 
     def draw(self, screen):
-        pygame.gfxdraw.filled_circle(screen, int(self.x), int(self.y), 10, PREDATOR_COLOR)
-        pygame.gfxdraw.aacircle(screen, int(self.x), int(self.y), 10, PREDATOR_COLOR)
+        pygame.gfxdraw.filled_circle(screen, int(self.x), int(self.y), 15, PREDATOR_COLOR)
+        pygame.gfxdraw.aacircle(screen, int(self.x), int(self.y), 15, PREDATOR_COLOR)
+
 
     def move(self):
         dx = PREDATOR_SPEED * math.cos(self.direction)
         dy = PREDATOR_SPEED * math.sin(self.direction)
         self.x += dx
         self.y += dy
-    
+
     def turn(self):
         self.direction += random.uniform(-math.pi / 16, math.pi / 16)
 
-plants = [Plant(random.randint(0, SCREEN_WIDTH), random.randint(0, SCREEN_HEIGHT)) for _ in range(10)]
+    def eat(self, herbivore):
+        distance = math.sqrt((self.x - herbivore.x) ** 2 + (self.y - herbivore.y) ** 2)
+        if distance <= 20:
+            self.energy += herbivore.energy
+            return True
+        else:
+            return False
+
+    def reproduce(self):
+        if self.energy >= PREDATOR_FOOD_THRESHOLD:
+            self.energy /= 2
+            return Predator(self.x, self.y, self.energy)
+        else:
+            return None
+
+plants = [Plant(random.randint(0, SCREEN_WIDTH), random.randint(0, SCREEN_HEIGHT), PLANT_ENERGY) for _ in range(10)]
 herbivores = [Herbivore(random.randint(0, SCREEN_WIDTH), random.randint(0, SCREEN_HEIGHT), HERBIVORE_ENERGY) for _ in range(5)]
 predators = [Predator(random.randint(0, SCREEN_WIDTH), random.randint(0, SCREEN_HEIGHT), PREDATOR_ENERGY) for _ in range(3)]
 
-while True:
+running = True
+while running:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:        
-            pygame.quit()
-            quit()
-    
+        if event.type == pygame.QUIT:
+            running = False
+
+    for plant in plants:
+        plant.grow()
+
+    for herbivore in herbivores:
+        herbivore.move()
+        herbivore.turn()
+
+        for plant in plants:
+            if herbivore.eat(plant):
+                plants.remove(plant)
+
+        new_herbivore = herbivore.reproduce()
+        if new_herbivore is not None:
+            herbivores.append(new_herbivore)
+
+        if herbivore.energy <= 0:
+            herbivores.remove(herbivore)
+
+    for predator in predators:
+        predator.move()
+        predator.turn()
+
+        for herbivore in herbivores:
+            if predator.eat(herbivore):
+                herbivores.remove(herbivore)
+
+        new_predator = predator.reproduce()
+        if new_predator is not None:
+            predators.append(new_predator)
+
+        if predator.energy <= 0:
+            predators.remove(predator)
+
     screen.fill(BACKGROUND_COLOR)
-        
+
     for plant in plants:
         plant.draw(screen)
 
     for herbivore in herbivores:
         herbivore.draw(screen)
-        herbivore.move()
-        herbivore.turn()
-    
+
     for predator in predators:
         predator.draw(screen)
-        predator.move()
-        predator.turn()
 
-    pygame.display.update()
+    pygame.display.flip()
     clock.tick(FPS)
-
+pygame.quit()
